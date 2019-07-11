69ceba8543d3b0c5e235c34f8e72e870f10b2eed
Gabriel Stein <gabrielstein@Gabriels-MacBook-Pro.local>
2019-07-11 13:39:48 +0200

    adding guards

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,9 @@ export const authSuccess = (token, userId) => {
 }
 
 export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('localId');
     return {
         type: actionTypes.AUTH_LOGOUT
     }
@@ -53,6 +56,10 @@ url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword
 
             axios.post(url, authData)
             .then(response => {
+                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+                localStorage.setItem('token',response.data.idToken)
+                localStorage.setItem('expirationDate', expirationDate)
+                localStorage.setItem('userId', response.data.localId)
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
                 dispatch(checkAuthTimeout(response.data.expiresIn));
             })
@@ -64,5 +71,22 @@ export const setAuthRedirectPath = (path) => {
     return {
         type: actionTypes.SET_AUTH_REDIRECT_PATH,
         path: path
+    }
+};
+
+export const authCheckState = () => {
+    return dispatch => {
+       const token = localStorage.getItem('token');
+       if(!token){
+           dispatch(logout());
+       }else {
+           const expirationDate = new Date(localStorage.getItem('expirationDate'));
+           if(expirationDate <= new Date()){
+            const userId = localStorage.getItem('userId');
+            dispatch(authSuccess(token, userId));
+            dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime())/1000));
+           }
+           
+       }
     }
 };
