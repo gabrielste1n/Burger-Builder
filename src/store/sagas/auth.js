8cec1c8afd402a570cecffa3e421cb99a9e00035
Gabriel Stein <gabrielstein@Gabriels-MacBook-Pro.local>
2019-07-14 16:59:01 +0200

    burgerBuilder side affetcs to sagas

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -23,10 +23,10 @@ export function* authUserSaga(action) {
     returnSecureToken: true
   };
   let url =
-    "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY";
+    "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyB31DZZrkJb7yww3FHfEwC5rxs36FCrFao";
   if (!action.isSignup) {
     url =
-      "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY";
+      "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyB31DZZrkJb7yww3FHfEwC5rxs36FCrFao";
   }
   try {
     const response = yield axios.post(url, authData);
